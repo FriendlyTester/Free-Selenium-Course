295b1b77d07ebc3ae84d1f229df9e7323e627fc2
Richard Bradshaw <richardbradshaw@gmail.com>
2017-08-25 12:10:04 +0100

    finished

--- java/src/test/java/lessons/D_Interactions.java
+++ java/src/test/java/lessons/D_Interactions.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class D_Interactions
